daaadc1d6343b259e7c24c9104a8fe57b2df8f3e
Precious Ezeigbo <preciousezeigbo81@gmail.com>
2024-12-15 14:40:02 +0100

    Updating

--- public/routes.py
+++ public/routes.py
@@ -3,9 +3,13 @@ from flask import Blueprint, render_template
 def register_routes(app, db, bcrypt):
     public_bp = Blueprint('public', __name__, template_folder='templates', static_folder='static')
 
-    @public_bp.route("/")
-    def home():
-        return render_template("home.html")
+    @public_bp.route('/')
+    def index():
+        return render_template("register.html")
+    
+    @public_bp.route('/login')
+    def login():
+        return render_template("login.html")
 
     # Register Blueprint with the app
     app.register_blueprint(public_bp)
