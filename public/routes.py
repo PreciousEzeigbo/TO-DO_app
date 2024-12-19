5a80848a25bdc2d1ac52a6f6936cf3e5ba35c964
Precious Ezeigbo <preciousezeigbo81@gmail.com>
2024-12-18 23:18:56 -0800

    Update

--- public/routes.py
+++ public/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Blueprint, Flask, render_template, request, redirect, url_for, flash, session
 from flask_login import login_user, logout_user, current_user, login_required, LoginManager
 from werkzeug.security import check_password_hash  # Import for checking hashed passwords
 
