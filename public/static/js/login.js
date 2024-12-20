e468a52fda9b4571f5f76a5b7383f9859ee517b7
Awesome6192 <christianchibuike14@gmail.com>
2024-12-20 13:10:44 +0100

    updated

--- public/static/js/login.js
+++ public/static/js/login.js
@@ -4,112 +4,66 @@ const usernameOrEmail = document.getElementById('identifier');
 const password = document.getElementById('password');
 
 
-// Check if the login form exists on the page
+// Check if the registration form exists on the page
 if (form) {
     // Add an event listener for the form's submit event
     form.addEventListener('submit', async (event) => {
         // Prevent the default form submission behavior
         event.preventDefault();
         // Validate form inputs
-        checkInputs();
+        if (checkInputs()) {
+            // Submit form if inputs are valid
+            form.submit();
+        }
     });
+}
 
-    function checkInputs() {
-        // Retrieve the values from the login form inputs
-        const identifierValue = usernameOrEmail.value.trim();
-        const passwordValue = password.value.trim();
+function checkInputs() {
+    // Retrieve the values from the login form inputs
+    const identifierValue = usernameOrEmail.value.trim();
+    const passwordValue = password.value.trim();
 
-        let valid = true;
+    let valid = true;
 
-        if (identifierValue === '') {
-            // Display error and add the error class
-            setErrorFor(usernameOrEmail, 'Username or Email cannot be blank');
-            valid = false;
-        } else {
-            // Display success class
-            setSuccessFor(usernameOrEmail);
-        }
-
-        if (passwordValue === '') {
-            // Display error and add the error class
-            setErrorFor(password, 'Password cannot be blank');
-            valid = false;
-        } else {
-            // Display success class
-            setSuccessFor(password);
-        }
-
-        // If form is valid, proceed with the login request
-        if (valid) {
-            submitForm(identifierValue, passwordValue);
-        }
+    if (identifierValue === '') {
+        // Display error and add the error class
+        setErrorFor(usernameOrEmail, 'Username or Email cannot be blank');
+        valid = false;
+    } else {
+        // Display success class
+        setSuccessFor(usernameOrEmail);
     }
 
-    async function submitForm(identifier, password) {
-        // Display loading gif
-        document.getElementById("loading").style.display = "block";
-        try {
-            // Send a POST request to the login endpoint with the form data
-            const response = await fetch("/api/users/login", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json', // Indicate JSON content
-                    'Accept': 'application/json' // Accept JSON responses
-                },
-                // Send form data as JSON
-                body: JSON.stringify({
-                    identifier: identifier,
-                    password: password
-                })
-            });
-
-            // Hide loading gif
-            document.getElementById("loading").style.display = "none";
-
-            if (response.ok) {
-                const data = await response.json();
-                const token = data.accessToken;
-
-                sessionStorage.setItem("token", token);
-
-
-                // Dispaly success message
-                document.getElementById("success-message").style.display = "block";
-                // Redirect to the home page on successful login after 4 seconds
-                setTimeout(() => {
-                    window.location.href = '/home.html';
-                }, 4000);
-            } else {
-                // Handle errors if login fails
-                const data = await response.json();
-                alert(data.errors[0]?.msg || 'Unknown error');
-            }
-        } catch (error) {
-            // Hide loading gif
-            document.getElementById("loading").style.display = "none";
-            // Handle network or unexpected errors
-            console.error('Error during login:', error);
-            alert('An error occurred while logging in.');
-        }
+    if (passwordValue === '') {
+        // Display error and add the error class
+        setErrorFor(password, 'Password cannot be blank');
+        valid = false;
+    } else {
+        // Display success class
+        setSuccessFor(password);
     }
 
-    // .form-control
-    function setErrorFor(input, message) {
-        const formControl = input.parentElement;
-        const small = formControl.querySelector('small')
+    // If form is valid, return valid
+    return valid;
+}
 
-        // Add the error class
-        formControl.className = 'form-control error';
 
-        // Add error message inside small
-        small.innerText = message;
-    }
+// .form-control
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
 
-    // .form-control
-    function setSuccessFor(input) {
-        const formControl = input.parentElement;
+    // Add the error class
+    formControl.className = 'form-control error';
 
-        // Add the success class
-        formControl.className = 'form-control success';
-    }
+    // Add error message inside small
+    small.innerText = message;
+}
+
+// .form-control
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    // Add the success class
+    formControl.className = 'form-control success';
 }
